c5e74da1d95a232006b4c4b05fbfcde104ff10d0
VinsenJovin <vinjovinda@gmail.com>
2024-07-16 18:17:02 +0700

    membenarkan file service

--- src/app/(home)/service/page.tsx
+++ src/app/(home)/service/page.tsx
@@ -3,14 +3,14 @@ import ServiceHero from "./components/hero";
 import Local from "./components/local";
 import Long from "./components/long";
 import ServiceSmall from "./components/service";
+import ServicePage from "./components/small";
 import Small from "./components/small";
 
 
 export default function Page() {
   return (
     <div className=" bg-white">
-      <Small/>
-
+      <ServicePage/>
     </div>
   );
 }
