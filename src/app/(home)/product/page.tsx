6d214b301dbd73824db8097c2a5c983fc589e989
VinsenJovin <vinjovinda@gmail.com>
2024-07-16 18:23:23 +0700

    memindahkan folder service

---
+++ src/app/(home)/product/page.tsx
@@ -0,0 +1,15 @@
+import Service from "../components/service";
+import ServiceHero from "../service/components/hero";
+import Local from "../service/components/local";
+import Long from "../service/components/long";
+import ServiceSmall from "./componentProduct/service";
+import ServicePage from "./componentProduct/small";
+import Small from "./componentProduct/small";
+
+export default function Page() {
+  return (
+    <div className=" bg-white">
+      <ServicePage />
+    </div>
+  );
+}
